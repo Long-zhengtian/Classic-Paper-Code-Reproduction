7b4e3beb76c742dd8d852363103fe4ccb7dd0b46
Long-zhengtian <2283727861@qq.com>
2022-09-16 15:26:32 +0800

    bug fixed 2

--- Social diversity promotes the emergence of cooperation in public goods games/Code-Reproduction/PGGE.py
+++ Social diversity promotes the emergence of cooperation in public goods games/Code-Reproduction/PGGE.py
@@ -11,19 +11,19 @@ from player import players, playersInit
 def PGG(x, NOCs, GorI, r):  # 以x为中心展开公共品博弈，此处代码可以优化，似乎有些问题？
     GoodsPool = 0  # 公共品
     if GorI:  # game
-        cost = AllCost * players[x].strategy
+        cost = c * players[x].strategy
         players[x].AccPayOffs -= cost
         GoodsPool += cost
         for y in NOCs.adj[x]:
-            cost = AllCost * players[y].strategy
+            cost = c * players[y].strategy
             players[y].AccPayOffs -= cost
             GoodsPool += cost
     else:  # individual
-        cost = AllCost / (NOCs.degree[x] + 1) * players[x].strategy
+        cost = c / (NOCs.degree[x] + 1) * players[x].strategy
         players[x].AccPayOffs -= cost
         GoodsPool += cost
         for y in NOCs.adj[x]:
-            cost = AllCost / (NOCs.degree[y] + 1) * players[y].strategy
+            cost = c / (NOCs.degree[y] + 1) * players[y].strategy
             players[y].AccPayOffs -= cost
             GoodsPool += cost
 
--- Social diversity promotes the emergence of cooperation in public goods games/Code-Reproduction/config.py
+++ Social diversity promotes the emergence of cooperation in public goods games/Code-Reproduction/config.py
@@ -1,8 +1,8 @@
 N = 400  # 节点数量
-PreStep = 3000  # 前置演化 10000
-CalStep = 1000  # PreStep之后演化CalStep取平均值 2000
-MeanStep = 25  # 100次运行结果取均值 100
+PreStep = 5000  # 前置演化 10000
+CalStep = 500  # PreStep之后演化CalStep取平均值 2000
+MeanStep = 10  # 100次运行结果取均值 100
 DiffGraph = 2  # 10中不同的图进行运算 10
 m = 2  # z=4
-AllCost = 1  # 合作者每次游戏的总贡献
+c = 1  # 合作者每次游戏的贡献c
 # r = 2  # 增强因子
